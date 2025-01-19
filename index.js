7f785eb5b6d7f5cce52cb9a54e2c113c92795868
Suyash Upadhyay <uranium235.a@gmail.com>
2025-01-19 18:03:33 +0530

    create template

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs"); //to get function for reading data , returns an object
 const { text } = require("stream/consumers");
 const http = require("http");
 const url = require("url");
+const path = require("path");
+const { log } = require("console");
 
 // --------------------------------------------------------------------------------
 //Blocking , synchronous way
@@ -30,29 +32,40 @@ const url = require("url");
 ///////////////////////////////////////////////////////////////////////////
 
 //Server : 
+
+    //top level code : code that is outside the callback function , is executed once we start the program 
+
+const data = fs.readFile(`${__dirname}/dev-data/data.json`,"utf-8")
+const dataObject = JSON.parse(data); 
+
 const server = http.createServer((req,res)=>{
     const pathName = req.url;
-
+    
     if(pathName==="/" || pathName === "/overview") {
-        res.end("hello from the server!");              //this is a response send to the client 
+        res.end("hello from the server!");         //this is a response send to the client
     }
     else if(pathName === "/product"){
         res.end("this is product page")
         console.log("this is the product");
     }
-
+    else if(pathName ==="/api") {         
+        res.writeHead(200,{
+            "Content-type":"application/json"
+        });
+        res.end(data)
+             
+    }
     else {
         res.writeHead(404,{
-            
-        }) //piece of response that we send back 
-        res.end("Page not found!");  // FIXED: added response for unknown routes
+            "Content-type": "text/html",   //Note header should be sent before the response
+        }) //piece of imformation about response that we send back
+        res.end("<h1>Page not found!</h1>");  // FIXED: added response for unknown routes
     }
-    
     console.log(req.url);
-
 });
   //whenever a request hits the server , this callback is called 
 
 server.listen(8000,"127.0.0.1",()=>{                      //callback runs whenever the server successfully starts
     console.log("Listning to the requests onn port 8000");
 })   //server starts listning for the incoming req , this  last callback runs whenever the server starts
+ 
